9837f6bffceb8ff2cbe0aeb7c20b42e4eb19060d
jburi <jacobbob55@comcast.net>
2019-10-11 22:04:08 -0500

    first

---
+++ Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Jacob Buri
+ * DestroyObjectX.cs
+ * Assignment 7
+ * Destroy particle after 2 seconds
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
